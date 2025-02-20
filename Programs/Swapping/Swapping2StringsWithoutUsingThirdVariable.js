c6f7445b31f266880dd6151372941c8c6dddb927
KripaNB <62209438+KripaNB@users.noreply.github.com>
2025-02-20 10:26:01 +0530

    Update Swapping2StringsWithoutUsingThirdVariable.js

--- Programs/Swapping/Swapping2StringsWithoutUsingThirdVariable.js
+++ Programs/Swapping/Swapping2StringsWithoutUsingThirdVariable.js
@@ -10,8 +10,8 @@ const prompt = require("prompt-sync")();
 
 var name1 = prompt("Enter a string: ");
 var name2 = prompt("Enter another string: ");
-console.log("The numbers before swapping : name1 = " + name1 + "; name2 = " + name2);
+console.log("The strings before swapping : name1 = " + name1 + "; name2 = " + name2);
 name1 = name1 + name2;
 name2 = name1.slice(0, name1.length - name2.length);
 name1 = name1.slice(name2.length);
-console.log("The numbers after swapping : name1 = " + name1 + "; name2 = " + name2);
+console.log("The strings after swapping : name1 = " + name1 + "; name2 = " + name2);
